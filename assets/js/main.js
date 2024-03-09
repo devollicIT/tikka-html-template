e4caf080c888a135a515fa1aab8031bc1b5b15f4
Ashfaqur Rahman <odesk.rahman@gmail.com>
2024-03-09 21:51:37 +0600

    Responsive issue fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,136 +1,155 @@
 $(document).ready(function () {
-  // Counter up
-  $("#counter-up").countMe(40, 2);
+   // Counter up
+   $("#counter-up").countMe(40, 2);
 
-  // Logo carousel
-  $(".logo_carousel").owlCarousel({
-    autoplay: true,
-    loop: true,
-    dots: false,
-    slideSpeed: 300,
-    margin: 10,
-    responsive: {
-      0: {
-        items: 1,
-        loop: true,
+   // Logo carousel
+   $(".logo_carousel").owlCarousel({
+      autoplay: true,
+      loop: true,
+      dots: false,
+      slideSpeed: 300,
+      margin: 10,
+      responsive: {
+         0: {
+            items: 1,
+            loop: true,
+         },
+         600: {
+            items: 3,
+            loop: true,
+         },
+         1000: {
+            items: 5,
+            loop: true,
+         },
       },
-      600: {
-        items: 3,
-        loop: true,
+   });
+
+   // Gallery carousel
+   $(".gallery-carousel").owlCarousel({
+      autoplay: true,
+      loop: true,
+      dots: false,
+      slideSpeed: 300,
+      margin: 20,
+      responsive: {
+         0: {
+            items: 1,
+            loop: true,
+         },
+         600: {
+            items: 2,
+            loop: true,
+         },
+         1000: {
+            items: 3,
+            loop: true,
+         },
       },
-      1000: {
-        items: 5,
-        loop: true,
+   });
+   // Gallery carousel
+   $(".project-details-images-wrapper").owlCarousel({
+      autoplay: true,
+      loop: true,
+      dots: false,
+      slideSpeed: 300,
+      margin: 20,
+      responsive: {
+         0: {
+            items: 1,
+            loop: true,
+         },
+         600: {
+            items: 2,
+            loop: true,
+         },
+         1000: {
+            items: 4,
+            loop: true,
+         },
       },
-    },
-  });
+   });
 
-  // Gallery carousel
-  $(".gallery-carousel").owlCarousel({
-    autoplay: true,
-    loop: true,
-    dots: false,
-    slideSpeed: 300,
-    margin: 20,
-    responsive: {
-      0: {
-        items: 1,
-        loop: true,
+   // Gallery carousel
+   const home2Testimonial = $(".home2-testimonial-quote-wrapper");
+   home2Testimonial.owlCarousel({
+      autoplay: true,
+      loop: true,
+      items: 1,
+      dots: false,
+      slideSpeed: 300,
+      margin: 20,
+      responsive: {
+         0: {
+            items: 1,
+            loop: true,
+         },
+         600: {
+            items: 1,
+            loop: true,
+         },
+         1000: {
+            items: 1,
+            loop: true,
+         },
       },
-      600: {
-        items: 2,
-        loop: true,
+   });
+
+   // Customer feedback carousel
+   const owl = $(".customers-feedback-carousel");
+   owl.owlCarousel({
+      autoPlay: true,
+      loop: true,
+      items: 2,
+      dots: false,
+      slideSpeed: 300,
+      margin: 20,
+      responsive: {
+         0: {
+            loop: true,
+            items: 1,
+         },
+         600: {
+            loop: true,
+            items: 1,
+         },
+         1000: {
+            items: 2,
+            loop: true,
+         },
       },
-      1000: {
-        items: 3,
-        loop: true,
-      },
-    },
-  })
+   });
 
-  // Gallery carousel
-  const home2Testimonial = $(".home2-testimonial-quote-wrapper");
-  home2Testimonial.owlCarousel({
-    autoplay: true,
-    loop: true,
-    items:1,
-    dots: false,
-    slideSpeed: 300,
-    margin: 20,
-    responsive: {
-      0: {
-        items: 1,
-        loop: true,
-      },
-      600: {
-        items: 1,
-        loop: true,
-      },
-      1000: {
-        items: 1,
-        loop: true,
-      },
-    },
-  })
+   // Owl prev slider
+   $(".prev-customer-feedback").click(function () {
+      owl.trigger("prev.owl.carousel");
+   });
 
+   // Home 2 testimonial prev slider
+   $(".home2-prev-customer-feedback").click(function () {
+      home2Testimonial.trigger("prev.owl.carousel");
+   });
 
-  // Customer feedback carousel
-  const owl = $(".customers-feedback-carousel");
-  owl.owlCarousel({
-    autoPlay: true,
-    loop: true,
-    items: 2,
-    dots: false,
-    slideSpeed: 300,
-    margin: 20,
-    responsive: {
-      0: {
-        loop: true,
-        items: 1,
-      },
-      600: {
-        loop: true,
-        items: 1,
-      },
-      1000: {
-        items: 2,
-        loop: true,
-      },
-    },
-  });
+   //   Owl next slider
+   $(".next-customer-feedback").click(function () {
+      owl.trigger("next.owl.carousel");
+   });
 
-  // Owl prev slider
-  $(".prev-customer-feedback").click(function () {
-    owl.trigger("prev.owl.carousel");
-  });
+   // New show more & less
+   $("#show__more--content").hide();
 
-  // Home 2 testimonial prev slider
-  $(".home2-prev-customer-feedback").click(function () {
-    home2Testimonial.trigger("prev.owl.carousel");
-  });
+   $("#show__more").click(function () {
+      $("#show__more--content").show(450);
+      $("#show__less").show();
+      $("#show__more").hide();
+   });
 
+   $("#show__less").click(function () {
+      $("#show__more--content").hide(450);
+      $("#show__more").show();
+      $(this).hide();
+   });
 
-  //   Owl next slider
-  $(".next-customer-feedback").click(function () {
-    owl.trigger("next.owl.carousel");
-  });
-
-  // New show more & less
-  $('#show__more--content').hide();
-
-  $('#show__more').click(function(){
-    $('#show__more--content').show(150);
-    $('#show__less').show();
-    $('#show__more').hide();
-  });
-
-  $('#show__less').click(function(){
-    $('#show__more--content').hide(150);
-    $('#show__more').show();
-    $(this).hide();
-  });
-
-    // Init AOS
-    AOS.init();
-
+   // Init AOS
+   AOS.init();
 });
