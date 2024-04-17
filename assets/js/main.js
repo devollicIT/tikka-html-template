ac8bfda94eae9f6427fef56f8a5878c3212a24bf
reformedTech01 <robin.rana@reformedtech.xyz>
2024-04-17 11:56:42 +0600

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -176,28 +176,13 @@ $(document).ready(function () {
 
    // Init AOS
    AOS.init();
- 
-   //  Counter 1
-   const counterUp = window.counterUp.default
-   const callback = entries => {
-      entries.forEach( entry => {
-         const el = entry.target
-         if ( entry.isIntersecting && ! el.classList.contains( 'is-visible' ) ) {
-            counterUp( el, {
-               duration: 2000,
-               delay: 10,
-            } )
-            el.classList.add( 'is-visible' )
-         }
-      } )
+});
+
+// Scroll to top
+$(window).scroll(function() {
+   if ($(this).scrollTop() > 400) {
+       $('.scroll-top').fadeIn();
+   } else {
+       $('.scroll-top').fadeOut();
    }
-   const IO = new IntersectionObserver( callback, { threshold: 1 } )
-   const el = document.querySelector('.counterup' )
-   IO.observe( el )
-   const el2 = document.querySelector('.counterup2' )
-   IO.observe( el2 )
-   const el3 = document.querySelector('.counterup3' )
-   IO.observe( el3 )
-   const el4 = document.querySelector('.counterup4' )
-   IO.observe( el4 )
 });
